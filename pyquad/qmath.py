3e22752b92b876621f1e5d2f7becaa0269f548a0
Rob <robert.j.farmer37@gmail.com>
2023-07-23 18:25:13 +0200

    Add (untested) python wrappers

--- pyquad/qmath.py
+++ pyquad/qmath.py
@@ -2,5 +2,547 @@ from .constant import *
 import qmathc as _qmathc
 
 
+
+
+def acos(x):
+    return _qmathc._acos(x)
+
+
+def acosq(x):
+    return _qmathc._acos(x)
+
+
+def acosh(x):
+    return _qmathc._acosh(x)
+
+
+def acoshq(x):
+    return _qmathc._acosh(x)
+
+
+def asin(x):
+    return _qmathc._asin(x)
+
+
+def asinq(x):
+    return _qmathc._asin(x)
+
+
+def asinh(x):
+    return _qmathc._asinh(x)
+
+
+def asinhq(x):
+    return _qmathc._asinh(x)
+
+
+def atan(x):
+    return _qmathc._atan(x)
+
+
+def atanq(x):
+    return _qmathc._atan(x)
+
+
+def atanh(x):
+    return _qmathc._atanh(x)
+
+
+def atanhq(x):
+    return _qmathc._atanh(x)
+
+
+def atan2(x,y):
+    return _qmathc._atan2(x,y)
+
+
+def atan2q(x,y):
+    return _qmathc._atan2(x,y)
+
+
+def cbrt(x):
+    return _qmathc._cbrt(x)
+
+
+def cbrtq(x):
+    return _qmathc._cbrt(x)
+
+
+def ceil(x):
+    return _qmathc._ceil(x)
+
+
+def ceilq(x):
+    return _qmathc._ceil(x)
+
+
+def copysign(x,y):
+    return _qmathc._copysign(x,y)
+
+
+def copysignq(x,y):
+    return _qmathc._copysign(x,y)
+
+
+def cosh(x):
+    return _qmathc._cosh(x)
+
+
+def coshq(x):
+    return _qmathc._cosh(x)
+
+
 def cos(x):
     return _qmathc._cos(x)
+
+
+def cosq(x):
+    return _qmathc._cos(x)
+
+
+def erf(x):
+    return _qmathc._erf(x)
+
+
+def erfq(x):
+    return _qmathc._erf(x)
+
+
+def erfc(x):
+    return _qmathc._erfc(x)
+
+
+def erfcq(x):
+    return _qmathc._erfc(x)
+
+
+def exp2(x):
+    return _qmathc._exp2(x)
+
+
+def exp2q(x):
+    return _qmathc._exp2(x)
+
+
+def exp(x):
+    return _qmathc._exp(x)
+
+
+def expq(x):
+    return _qmathc._exp(x)
+
+
+def expm1(x):
+    return _qmathc._expm1(x)
+
+
+def expm1q(x):
+    return _qmathc._expm1(x)
+
+
+def fabs(x):
+    return _qmathc._fabs(x)
+
+
+def fabsq(x):
+    return _qmathc._fabs(x)
+
+
+def fdim(x,y):
+    return _qmathc._fdim(x,y)
+
+
+def fdimq(x,y):
+    return _qmathc._fdim(x,y)
+
+
+def finite(x):
+    return _qmathc._finite(x)
+
+
+def finiteq(x):
+    return _qmathc._finite(x)
+
+
+def floor(x):
+    return _qmathc._floor(x)
+
+
+def floorq(x):
+    return _qmathc._floor(x)
+
+
+def fma(x,y,z):
+    return _qmathc._fma(x,y,z)
+
+
+def fmaq(x,y):
+    return _qmathc._fma(x,y)
+
+
+def fmax(x,y):
+    return _qmathc._fmax(x,y)
+
+
+def fmaxq(x,y):
+    return _qmathc._fmax(x,y)
+
+
+def fmin(x,y):
+    return _qmathc._fmin(x,y)
+
+
+def fminq(x,y):
+    return _qmathc._fmin(x,y)
+
+
+def fmod(x,y):
+    return _qmathc._fmod(x,y)
+
+
+def fmodq(x,y):
+    return _qmathc._fmod(x,y)
+
+
+# def frexp(x):
+#     return _qmathc._frexp(x)
+
+
+# def frexpq(x):
+#     return _qmathc._frexp(x)
+
+
+def hypot(x,y):
+    return _qmathc._hypot(x,y)
+
+
+def hypotq(x):
+    return _qmathc._hypot(x)
+
+
+def ilogb(x):
+    return _qmathc._ilogb(x)
+
+
+def ilogbq(x):
+    return _qmathc._ilogb(x)
+
+
+def isinf(x):
+    return _qmathc._isinf(x)
+
+
+def isinfq(x):
+    return _qmathc._isinf(x)
+
+
+def isnan(x):
+    return _qmathc._isnan(x)
+
+
+def isnanq(x):
+    return _qmathc._isnan(x)
+
+
+def issignaling(x):
+    return _qmathc._issignaling(x)
+
+
+def issignalingq(x):
+    return _qmathc._issignaling(x)
+
+
+def j0(x):
+    return _qmathc._j0(x)
+
+
+def j0q(x):
+    return _qmathc._j0(x)
+
+
+def j1(x):
+    return _qmathc._j1(x)
+
+
+def j1q(x):
+    return _qmathc._j1(x)
+
+
+def jn(x):
+    return _qmathc._jn(x)
+
+
+def jnq(x):
+    return _qmathc._jn(x)
+
+
+# def ldexp(x):
+#     return _qmathc._ldexp(x)
+
+
+# def ldexpq(x):
+#     return _qmathc._ldexp(x)
+
+
+def lgamma(x):
+    return _qmathc._lgamma(x)
+
+
+def lgammaq(x):
+    return _qmathc._lgamma(x)
+
+
+def llrint(x):
+    return _qmathc._llrint(x)
+
+
+def llrintq(x):
+    return _qmathc._llrint(x)
+
+
+def llround(x):
+    return _qmathc._llround(x)
+
+
+def llroundq(x):
+    return _qmathc._llround(x)
+
+
+def logb(x):
+    return _qmathc._logb(x)
+
+
+def logbq(x):
+    return _qmathc._logb(x)
+
+
+def log(x):
+    return _qmathc._log(x)
+
+
+def logq(x):
+    return _qmathc._log(x)
+
+
+def log10(x):
+    return _qmathc._log10(x)
+
+
+def log10q(x):
+    return _qmathc._log10(x)
+
+
+def log1p(x):
+    return _qmathc._log1p(x)
+
+
+def log1pq(x):
+    return _qmathc._log1p(x)
+
+
+def log2(x):
+    return _qmathc._log2(x)
+
+
+def log2q(x):
+    return _qmathc._log2(x)
+
+
+def lrint(x):
+    return _qmathc._lrint(x)
+
+
+def lrintq(x):
+    return _qmathc._lrint(x)
+
+
+def lround(x):
+    return _qmathc._lround(x)
+
+
+def lroundq(x):
+    return _qmathc._lround(x)
+
+
+# def modf(x):
+#     return _qmathc._modf(x)
+
+
+# def modfq(x):
+#     return _qmathc._modf(x)
+
+
+# def nan(x):
+#     return _qmathc._nan(x)
+
+
+# def nanq(x):
+#     return _qmathc._nan(x)
+
+
+def nearbyint(x):
+    return _qmathc._nearbyint(x)
+
+
+def nearbyintq(x):
+    return _qmathc._nearbyint(x)
+
+
+def nextafter(x,y):
+    return _qmathc._nextafter(x,y)
+
+
+def nextafterq(x,y):
+    return _qmathc._nextafter(x,y)
+
+
+def pow(x,y):
+    return _qmathc._pow(x,y)
+
+
+def powq(x,y):
+    return _qmathc._pow(x,y)
+
+
+def remainder(x,y):
+    return _qmathc._remainder(x,y)
+
+
+def remainderq(x,y):
+    return _qmathc._remainder(x,y)
+
+
+# def remquo(x):
+#     return _qmathc._remquo(x)
+
+
+# def remquoq(x):
+#     return _qmathc._remquo(x)
+
+
+def rint(x):
+    return _qmathc._rint(x)
+
+
+def rintq(x):
+    return _qmathc._rint(x)
+
+
+def round(x):
+    return _qmathc._round(x)
+
+
+def roundq(x):
+    return _qmathc._round(x)
+
+
+# def scalbln(x):
+#     return _qmathc._scalbln(x)
+
+
+# def scalblnq(x):
+#     return _qmathc._scalbln(x)
+
+
+# def scalbn(x):
+#     return _qmathc._scalbn(x)
+
+
+# def scalbnq(x):
+#     return _qmathc._scalbn(x)
+
+
+def signbit(x):
+    return _qmathc._signbit(x)
+
+
+def signbitq(x):
+    return _qmathc._signbit(x)
+
+
+# def sincos(x):
+#     return _qmathc._sincos(x)
+
+
+# def sincosq(x):
+#     return _qmathc._sincos(x)
+
+
+def sinh(x):
+    return _qmathc._sinh(x)
+
+
+def sinhq(x):
+    return _qmathc._sinh(x)
+
+
+def sin(x):
+    return _qmathc._sin(x)
+
+
+def sinq(x):
+    return _qmathc._sin(x)
+
+
+def sqrt(x):
+    return _qmathc._sqrt(x)
+
+
+def sqrtq(x):
+    return _qmathc._sqrt(x)
+
+
+def tan(x):
+    return _qmathc._tan(x)
+
+
+def tanq(x):
+    return _qmathc._tan(x)
+
+
+def tanh(x):
+    return _qmathc._tanh(x)
+
+
+def tanhq(x):
+    return _qmathc._tanh(x)
+
+
+def tgamma(x):
+    return _qmathc._tgamma(x)
+
+
+def tgammaq(x):
+    return _qmathc._tgamma(x)
+
+
+def trunc(x):
+    return _qmathc._trunc(x)
+
+
+def truncq(x):
+    return _qmathc._trunc(x)
+
+
+def y0(x):
+    return _qmathc._y0(x)
+
+
+def y0q(x):
+    return _qmathc._y0(x)
+
+
+def y1(x):
+    return _qmathc._y1(x)
+
+
+def y1q(x):
+    return _qmathc._y1(x)
+
+
+# def yn(x):
+#     return _qmathc._yn(x)
+
+
+# def ynq(x):
+#     return _qmathc._yn(x)
