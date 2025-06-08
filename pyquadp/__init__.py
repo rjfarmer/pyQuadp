6b96786bb0940be6d29f071433ac556873e2e64a
Rob Farmer <robert.j.farmer37@gmail.com>
2025-06-08 21:51:03 +0100

    Fix pickling

--- pyquadp/__init__.py
+++ pyquadp/__init__.py
@@ -5,3 +5,10 @@ from pyquadp.qmint import qint
 from pyquadp.qmcmplx import qcmplx
 
 from pyquadp.constant import *
+
+import builtins
+
+# Make things pickle importable
+builtins.qint = qint
+builtins.qfloat = qfloat
+builtins.qcmplx = qcmplx
